fe76c919967e5c7525bf36385f10afcac5303cce
med aziz nefzi <medaziznefzi1@gmail.com>
2025-02-28 01:24:46 +0100

    update 4.5.6
    navbar update  design and features

--- database/seeders/GradeSeeder.php
+++ database/seeders/GradeSeeder.php
@@ -10,17 +10,17 @@ class GradeSeeder extends Seeder
     public function run(): void
     {
         $grades = [
-            ['name_fr' => 'Sergent', 'name_ar' => 'عريف', 'image_path' => 'grade/srg.png'],
-            ['name_fr' => 'Sergent-chef', 'name_ar' => 'عريف أول', 'image_path' => 'grade/srg-chef.png'],
-            ['name_fr' => 'Adjoint', 'name_ar' => 'وكيل', 'image_path' => 'grade/adj.png'],
-            ['name_fr' => 'Adjoint-chef', 'name_ar' => 'وكيل أول', 'image_path' => 'grade/adj-chef.png'],
-            ['name_fr' => 'Sous-lieutenant', 'name_ar' => 'ملازم', 'image_path' => 'grade/slt.png'],
-            ['name_fr' => 'Lieutenant', 'name_ar' => 'ملازم أول', 'image_path' => 'grade/lt.png'],
-            ['name_fr' => 'Capitaine', 'name_ar' => 'نقيب', 'image_path' => 'grade/cpt.png'],
-            ['name_fr' => 'Commandant', 'name_ar' => 'رائد', 'image_path' => 'grade/cdt.png'],
-            ['name_fr' => 'Lieutenant-colonel', 'name_ar' => 'مقدم', 'image_path' => 'grade/col-lt.png'],
-            ['name_fr' => 'Colonel plein', 'name_ar' => 'عقيد', 'image_path' => 'grade/col-plein.png'],
-            ['name_fr' => 'Colonel-major', 'name_ar' => 'عميد', 'image_path' => 'grade/col-major.png'],
+            ['name_fr' => 'Sergent', 'name_ar' => 'عريف', 'image_path' => 'grades/srg.png'],
+            ['name_fr' => 'Sergent-chef', 'name_ar' => 'عريف أول', 'image_path' => 'grades/srg-chef.png'],
+            ['name_fr' => 'Adjoint', 'name_ar' => 'وكيل', 'image_path' => 'grades/adj.png'],
+            ['name_fr' => 'Adjoint-chef', 'name_ar' => 'وكيل أول', 'image_path' => 'grades/adj-chef.png'],
+            ['name_fr' => 'Sous-lieutenant', 'name_ar' => 'ملازم', 'image_path' => 'grades/slt.png'],
+            ['name_fr' => 'Lieutenant', 'name_ar' => 'ملازم أول', 'image_path' => 'grades/lt.png'],
+            ['name_fr' => 'Capitaine', 'name_ar' => 'نقيب', 'image_path' => 'grades/cpt.png'],
+            ['name_fr' => 'Commandant', 'name_ar' => 'رائد', 'image_path' => 'grades/cdt.png'],
+            ['name_fr' => 'Lieutenant-colonel', 'name_ar' => 'مقدم', 'image_path' => 'grades/col-lt.png'],
+            ['name_fr' => 'Colonel plein', 'name_ar' => 'عقيد', 'image_path' => 'grades/col-plein.png'],
+            ['name_fr' => 'Colonel-major', 'name_ar' => 'عميد', 'image_path' => 'grades/col-major.png'],
             
             
         ];
--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -2,62 +2,100 @@
   <!-- Top Navbar -->
   <nav class="navbar top-navbar">
     <div class="container">
-      <div class="navbar-content">
+      <div class="navbar-content d-flex justify-content-between align-items-center">
+        <!-- Left Section: Brand -->
         <a href="#" class="navbar-brand">
-        ح.م<span>.د.و</span>
+          ح.م<span>.د.و</span>
         </a>
-        <ul class="navbar-nav">
-          <!-- Notification Dropdown -->
-          <li class="nav-item dropdown">
-              @include('partials.notifications')
-          </li>
-          <!-- Profile Dropdown -->
-          <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle" href="#" id="profileDropdown" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-              <img class="wd-30 ht-30 rounded-circle" src="{{ asset('assets/images/users/player.png') }}" alt="profile">
-            </a>
-            <div class="dropdown-menu p-0" aria-labelledby="profileDropdown">
-              <div class="d-flex flex-column align-items-center border-bottom px-5 py-3">
-                <div class="mb-3">
-                  <img class="wd-80 ht-80 rounded-circle" src="{{ asset('assets/images/users/player.png') }}" alt="">
-                </div>
-                <div class="text-center">
-                  <p class="tx-16 fw-bolder">{{ Auth::user()->name }}</p>
-                  <p class="tx-12 text-muted">{{ Auth::user()->email }}</p>
-                  <!-- Display CIN under email -->
-                  <p class="tx-12 text-muted">{{ Auth::user()->cin }}</p>
+
+        <!-- Center Section: Grade Display -->
+        @if(Auth::check() && (Auth::user()->role == 1 || Auth::user()->role == 2))
+          <div class="d-flex justify-content-center flex-grow-1">
+            @if(Auth::user()->grade)
+              <div class="grade-display p-2 px-4 d-flex align-items-center flex-wrap" style=" 
+                @if(Auth::user()->role == 1)
+                  background-color:rgb(255, 208, 245);
+                @elseif(Auth::user()->role == 2)
+                  background-color:rgb(255, 133, 133);
+                @endif
+                border-radius: 4px;
+                max-width: fit-content; /* Ensure the width fits the content */
+                height: fit-content; /* Ensure the height fits the content */
+                gap: 18px; /* Add space between the image and text */
+              ">
+                <!-- Grade image: smaller and not rounded -->
+                <img src="{{ asset(Auth::user()->grade->image_path) }}" alt="Grade Image" style="max-width: 28px; max-height: 28px;">
+                <div class="grade-text">
+                  @if(Auth::user()->role == 1)
+                    جهوي 
+                  @elseif(Auth::user()->role == 2)
+                    مركزي 
+                  @endif
                 </div>
               </div>
-              <ul class="list-unstyled p-1">
-                <li class="dropdown-item py-2">
-                  <a href="pages/general/profile.html" class="text-body ms-0">
-                    <i class="me-2 icon-md" data-feather="user"></i>
-                    <span>حساب</span>
-                  </a>
-                </li>
-                <li class="dropdown-item py-2">
-                  <a href="javascript:;" class="text-body ms-0">
-                    <i class="me-2 icon-md" data-feather="edit"></i>
-                    <span> تعديل حساب </span>
-                  </a>
-                </li>
-                <li class="dropdown-item py-2">
-                  <!-- Logout Form -->
-                  <form id="logout-form" action="{{ route('logout') }}" method="POST">
-                    @csrf
-                    <button type="submit" class="text-body ms-0" style="border: none; background: none; padding: 0; margin: 0;">
-                      <i class="me-2 icon-md" data-feather="log-out"></i>
-                      <span>تسجيل خروج</span>
-                    </button>
-                  </form>
-                </li>
-              </ul>
-            </div>
-          </li>
-        </ul>
-        <button class="navbar-toggler navbar-toggler-right d-lg-none align-self-center" type="button" data-toggle="horizontal-menu-toggle">
-          <i data-feather="menu"></i>
-        </button>
+            @endif
+          </div>
+        @endif
+
+
+
+        <!-- Right Section: Notifications & Profile -->
+        <div class="d-flex align-items-center">
+          <ul class="navbar-nav">
+            <!-- Notification Dropdown -->
+            <li class="nav-item dropdown">
+                @include('partials.notifications')
+            </li>
+            <!-- Profile Dropdown -->
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="#" id="profileDropdown" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                <img class="wd-30 ht-30 rounded-circle" src="{{ asset('assets/images/users/player.png') }}" alt="profile">
+              </a>
+              <div class="dropdown-menu p-0" aria-labelledby="profileDropdown">
+                <div class="d-flex flex-column align-items-center border-bottom px-5 py-3">
+                  <div class="mb-3">
+                    <img class="wd-80 ht-80 rounded-circle" src="{{ asset('assets/images/users/player.png') }}" alt="">
+                  </div>
+                  <div class="text-center">
+                    <p class="tx-16 fw-bolder">{{ Auth::user()->name }}</p>
+                    @if(Auth::user()->role == 1)
+                    <p class="tx-12 text-muted">{{ Auth::user()->governorate ? Auth::user()->governorate->name : '' }}</p>
+                    @endif
+                    <p class="tx-12 text-muted">{{ Auth::user()->email }}</p>
+                    <p class="tx-12 text-muted">{{ Auth::user()->cin }}</p>
+                  </div>
+                </div>
+                <ul class="list-unstyled p-1">
+                  <li class="dropdown-item py-2">
+                    <a href="pages/general/profile.html" class="text-body ms-0">
+                      <i class="me-2 icon-md" data-feather="user"></i>
+                      <span>حساب</span>
+                    </a>
+                  </li>
+                  <li class="dropdown-item py-2">
+                    <a href="javascript:;" class="text-body ms-0">
+                      <i class="me-2 icon-md" data-feather="edit"></i>
+                      <span> تعديل حساب </span>
+                    </a>
+                  </li>
+                  <li class="dropdown-item py-2">
+                    <form id="logout-form" action="{{ route('logout') }}" method="POST">
+                      @csrf
+                      <button type="submit" class="text-body ms-0" style="border: none; background: none; padding: 0; margin: 0;">
+                        <i class="me-2 icon-md" data-feather="log-out"></i>
+                        <span>تسجيل خروج</span>
+                      </button>
+                    </form>
+                  </li>
+                </ul>
+              </div>
+            </li>
+          </ul>
+
+          <button class="navbar-toggler navbar-toggler-right d-lg-none align-self-center" type="button" data-toggle="horizontal-menu-toggle">
+            <i data-feather="menu"></i>
+          </button>
+        </div>
       </div>
     </div>
   </nav>
@@ -74,7 +112,7 @@
         </li>
         
         @if(Auth::user()->role == 0)
-        <!-- hedha mte3 talab chhedet wiqaya -->
+        <!-- طلبات شهادة وقاية وتكوين للعموم -->
         <li class="nav-item">
           <a href="/prev" class="nav-link">
             <i class="link-icon" data-feather="file-text"></i>
@@ -90,8 +128,7 @@
         </li>
         @endif
 
-
-        <!-- hedha mte3 moutabe3et el mataleb -->
+        <!-- متابعة المطالب -->
         @if(Auth::user()->role == 1 || Auth::user()->role == 2)
         <li class="nav-item">
           <a class="nav-link" href="#">
@@ -112,7 +149,7 @@
         <li class="nav-item">
           <a class="nav-link" href="#">
             <i class="link-icon" data-feather="layers"></i>
-            <span class="menu-title"> التكوين و الرسكلة</span>
+            <span class="menu-title">التكوين و الرسكلة</span>
             <i class="link-arrow"></i>
           </a>
           <div class="submenu">
@@ -123,8 +160,7 @@
         </li>
         @endif
 
-
-        <!-- hedha mte3 el user management -->
+        <!-- إدارة المستخدمين -->
         @if(Auth::user()->role == 2)
         <li class="nav-item">
           <a class="nav-link" href="/management">
@@ -134,8 +170,7 @@
         </li>
         @endif
 
-
-        <!-- hedhy khaleha zeyda akeka -->
+        <!-- ONPC -->
         <li class="nav-item">
           <a href="#" class="nav-link">
             <i class="link-icon" data-feather="hash"></i>
